ffd19b9c66e42fa7008696b38aad372c46825adc
fransurf <flcknight@gmail.com>
2022-08-04 12:48:17 +0100

    tech added to projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -60,6 +60,21 @@ const Projects = () => {
     <>
       <Container id='projects-container'>
         <h1>All projects {'>>>'}</h1>
+        <div className='skills'>
+          <div className='tech' id='html'></div>
+          <div className='tech' id='css'></div>
+          <div className='tech' id='js'></div>
+          <div className='tech' id='boots'></div>
+          <div className='tech' id='sass'></div>
+          <div className='tech' id='react'></div>
+          <div className='tech' id='python'></div>
+          <div className='tech' id='django'></div>
+          <div className='tech' id='post'></div>
+          <div className='tech' id='mongo'></div>
+          <div className='tech' id='node'></div>
+          <div className='tech' id='express'></div>
+        </div>
+
         <div className='proj-selectors img-left' id='proj4'>
 
           <div className='content-outer' onClick={handleClick}>
