39794f21db7876b1350517b49ffa8c09c38a39c9
fransurf <flcknight@gmail.com>
2022-08-11 17:31:23 +0100

    removed portfolio-fk from navbar to go back to homepage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
       <nav id='navbar' >
           <Link className='link' to='/about'>About</Link>
           <Link className='link' to='/projects'>Projects</Link>
-          <Link className='link' to='/portfolio-fk'>FK</Link>
+          <Link className='link' to='/'>FK</Link>
           <Link className='link' to='/academics'>Academics</Link>
           <Link className='link' to='/contact'>Contact</Link>
       </nav>
