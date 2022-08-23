c8a6b96fbcb93bf8cacce6ce3dc0ff7412b2f9b9
fransurf <flcknight@gmail.com>
2022-08-23 17:31:12 +0100

    Page loading animation added

--- src/components/About.js
+++ src/components/About.js
@@ -1,61 +1,75 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from 'react-bootstrap/Container'
-// import { Link } from 'react-router-dom'
+
+import Loading from '../helpers/Loading.js'
 
 
 const About = () => {
 
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3000)
+  }, [])
+
   return (
     <>
-      <Container className='content-container' id='about-container'>
+      {loading === false ? (
+        <Container className='content-container' id='about-container'>
 
-        {/* TITLE */}
-        <h1 className='page-title' id='about-title'>About me<span>.</span> </h1>
+          {/* TITLE */}
+          <h1 className='page-title' id='about-title'>About me<span>.</span> </h1>
 
-        {/* CONTENT IN 3 DIVS WITH IMAGE OVERLAY REVEALING TEXT ON HOVER */}
-        <div className='about-content'>
-          <div className='about-div expand' id='se'>
-            <h2 className='about-heading' id='se-text'><span>.</span>sofware engineer</h2>
-            <div className='about-top'>
-              <div className='top-contents img-overlay' id='se-img'></div>
-              <div className=' top-contents about-text'>
-                <p>{`My particular fixation in software engineering is on producing well-structured content in beautiful & fun, dynamic interfaces.`} <br></br><br></br> {`I take the same approach with my code: I don't believe the shortest code is the best. I aim to produce readable code that any body can quickly manoeuvre.`} <br></br><br></br> {`As a software engineer, I am always learning. If there's a problem to solve, you can call me Poirot`}</p>
+          {/* CONTENT IN 3 DIVS WITH IMAGE OVERLAY REVEALING TEXT ON HOVER */}
+          <div className='about-content'>
+            <div className='about-div expand' id='se'>
+              <h2 className='about-heading' id='se-text'><span>.</span>sofware engineer</h2>
+              <div className='about-top'>
+                <div className='top-contents img-overlay' id='se-img'></div>
+                <div className=' top-contents about-text'>
+                  <p>{`My particular fixation in software engineering is on producing well-structured content in beautiful & fun, dynamic interfaces.`} <br></br><br></br> {`I take the same approach with my code: I don't believe the shortest code is the best. I aim to produce readable code that any body can quickly manoeuvre.`} <br></br><br></br> {`As a software engineer, I am always learning. If there's a problem to solve, you can call me Poirot`}</p>
+                </div>
               </div>
+              {/* <Link to='/projects' className='about-link'>Projects</Link> */}
             </div>
-            {/* <Link to='/projects' className='about-link'>Projects</Link> */}
+
+            <div className='about-div expand' id='neuro'>
+              <h2 className='about-heading' id='neuro-text'><span>.</span>neuroscientist</h2>
+              <div className='about-top'>
+                <div className='img-overlay' id='neuro-img'></div>
+                <div className='about-text'>
+                  <p>{`In my heart, I'm an uber geek. And early on, my heart found neuroscience.`} <br></br><br></br> {`Understanding human behaviour in terms of the network of interactions in the brain fascinates me. And I love passing that passion on.`} <br></br><br></br> {`Teaching neuroscience has taught me a lot about the importance of engaging a person's interest & breaking down information in order to maximise attention & learning.`}</p>
+                </div>
+              </div>
+              {/* <Link to='/academics' className='about-link'>Academics</Link> */}
+            </div>
+
+            <div className='about-div expand' id='snowboarder'>
+              <h2 className='about-heading' id='snowboarder-text'><span>.</span>snowboarder</h2>
+              <div className='about-top'>
+
+                <div className='img-overlay' id='snowboarder-img'></div>
+                <div className='about-text'>
+                  <p>{`My happiest place is on the mountains or the waves. I am a dedicated snowboarder & recently took a 6-month career break to transfer those skills onto the waves in Sri Lanka. I find the experience of being at the mercy of mother nature both exhilarating & humbling.`} <br></br><br></br> {` Outside the extreme, I love camping (the boot of my car is like an extension of Mountain Warehouse), running, yoga, dinner with friends, pub quizzes & a good series.`}</p>
+                </div>
+              </div>
+              {/* <Link to='/indulge' className='about-link'>Shall we indulge?</Link> */}
+            </div>
           </div>
 
-          <div className='about-div expand' id='neuro'>
-            <h2 className='about-heading' id='neuro-text'><span>.</span>neuroscientist</h2>
-            <div className='about-top'>
-              <div className='img-overlay' id='neuro-img'></div>
-              <div className='about-text'>
-                <p>{`In my heart, I'm an uber geek. And my heart found neuroscience.`} <br></br><br></br> {`Understanding human behaviour in terms of the network of interactions in the brain fascinates me. And I love passing that passion on.`} <br></br><br></br> {`Teaching neuroscience has taught me a lot about the importance of engaging a person's interest & breaking down information in order to maximise attention & learning.`}</p>
-              </div>
-            </div>
-            {/* <Link to='/academics' className='about-link'>Academics</Link> */}
+          {/* SCROLL BAR - PRESENTING TECH SKILLS */}
+          <div className='scroll-bar'>
+            <div className='scroll-content'> HTML5 <span>|</span> CSS <span>|</span> JavaScript <span>|</span> Sass <span>|</span> Bootstrap <span>|</span> React <span>|</span> Python <span>|</span> Django <span>|</span> PostgreSQL <span>|</span> Node.js <span>|</span> Axios <span>|</span> Express.js <span>|</span> Insomnia <span>|</span> Yarn <span>|</span> npm <span>|</span> HTML5 <span>|</span> CSS <span>|</span> JavaScript <span>|</span> Sass <span>|</span> Bootstrap <span>|</span> React <span>|</span> Python <span>|</span> Django <span>|</span> PostgreSQL <span>|</span> Node.js <span>|</span> Axios <span>|</span> Express.js <span>|</span> Insomnia <span>|</span> Yarn <span>|</span> npm <span>|</span></div>
           </div>
 
-          <div className='about-div expand' id='snowboarder'>
-            <h2 className='about-heading' id='snowboarder-text'><span>.</span>snowboarder</h2>
-            <div className='about-top'>
 
-              <div className='img-overlay' id='snowboarder-img'></div>
-              <div className='about-text'>
-                <p>{`My happiest place is on the mountains or the waves. I am a dedicated snowboarder & recently took a 6-month career break to transfer those skills onto the waves in Sri Lanka. I find the experience of being at the mercy of mother nature both exhilarating & humbling.`} <br></br><br></br> {` Outside of the extreme, I love camping (the boot of my car is like an extension of Mountain Warehouse), running, yoga, dinner with friends, pub quizzes & a good series.`}</p>
-              </div>
-            </div>
-            {/* <Link to='/indulge' className='about-link'>Shall we indulge?</Link> */}
-          </div>
-        </div>
+        </Container>
 
-      {/* SCROLL BAR - PRESENTING TECH SKILLS */}
-        <div className='scroll-bar'>
-          <div className='scroll-content'> HTML5 <span>|</span> CSS <span>|</span> JavaScript <span>|</span> Sass <span>|</span> Bootstrap <span>|</span> React <span>|</span> Python <span>|</span> Django <span>|</span> PostgreSQL <span>|</span> Node.js <span>|</span> Axios <span>|</span> Express.js <span>|</span> Insomnia <span>|</span> Yarn <span>|</span> npm <span>|</span> HTML5 <span>|</span> CSS <span>|</span> JavaScript <span>|</span> Sass <span>|</span> Bootstrap <span>|</span> React <span>|</span> Python <span>|</span> Django <span>|</span> PostgreSQL <span>|</span> Node.js <span>|</span> Axios <span>|</span> Express.js <span>|</span> Insomnia <span>|</span> Yarn <span>|</span> npm <span>|</span></div>
-        </div>
+      ) : (
 
+        <Loading />
 
-      </Container>
+      )}
 
       <div className='overlay' id='about'></div>
       {/* <div className='overlay2'></div> */}
---
+++ src/helpers/Loading.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Loading = () => (
+
+  <div className="wrap">
+    <div className="loading">
+
+      <div className="text">Bear with me a moment</div>
+      <div className="bounceball"></div>
+      <div className="bounceball delay-1"></div>
+      <div className="bounceball delay-2"></div>
+
+    </div>
+  </div>
+
+)
+
+export default Loading
