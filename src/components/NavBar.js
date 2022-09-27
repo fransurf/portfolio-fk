726044a153de28bf7485da29c0361377ac3394e0
fransurf <flcknight@gmail.com>
2022-09-27 15:37:22 +0100

    hamburger menu transition from side

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,14 +12,19 @@ const NavBar = () => {
 // * REVEAL DROPDOWN MENU ON HAMBURGER CLICK
   const menuReveal = () => {
     let menu = document.getElementById("hamburger-menu")
-    if (menu.style.display === 'flex') {
-      menu.style.display = 'none'
-    } else {
-      menu.style.display = 'flex'
-    }
+    menu.classList.toggle('active')
 
     let hamburger = document.getElementById("hamburger")
     hamburger.classList.toggle('active')
+  }
+
+  // * REMOVE DROPDOWN ON NAVIGATION TO NEW PAGE
+  const menuDisappear = () => {
+    let menu = document.getElementById("hamburger-menu")
+    menu.classList.remove('active')
+
+    let hamburger = document.getElementById("hamburger")
+    hamburger.classList.remove('active')
   }
 
   return (
@@ -33,12 +38,12 @@ const NavBar = () => {
       </nav>
 
       <nav id='navbar-mobile' className='navbar'>
-        <Link className='hamburger-link link' to='/'><img src={Logo} alt='FK logo' className='logo' /></Link>
+        <Link className='hamburger-link link' to='/' onClick={menuDisappear}><img src={Logo} alt='FK logo' className='logo' /></Link>
         <div id='hamburger-menu' className='hamburger-menu'>
-          <Link className='hamburger-link' to='/about'>About</Link>
-          <Link className='hamburger-link' to='/projects'>Projects</Link>
-          <Link className='hamburger-link' to='/academics'>Academics</Link>
-          <Link className='hamburger-link' to='/contact'>Contact</Link>
+          <Link className='hamburger-link' to='/about' onClick={menuDisappear}>About</Link>
+          <Link className='hamburger-link' to='/projects' onClick={menuDisappear}>Projects</Link>
+          <Link className='hamburger-link' to='/academics' onClick={menuDisappear}>Academics</Link>
+          <Link className='hamburger-link' to='/contact' onClick={menuDisappear}>Contact</Link>
         </div>
         <div id='hamburger' onClick={menuReveal} className='icon'>
           <div className='burger'></div>
